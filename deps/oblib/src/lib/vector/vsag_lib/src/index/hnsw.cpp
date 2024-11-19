ad0aaf55e725b79b55efba2ee8ef424a50c5d913
名为a的全局变量 <1051233107@qq.com>
2024-11-19 18:35:07 +0800

    try M=24，ef=120

--- deps/oblib/src/lib/vector/vsag_lib/src/index/hnsw.cpp
+++ deps/oblib/src/lib/vector/vsag_lib/src/index/hnsw.cpp
@@ -74,8 +74,8 @@ HNSW::HNSW(std::shared_ptr<hnswlib::SpaceInterface> space_interface,
     allocator_ = std::shared_ptr<SafeAllocator>(new SafeAllocator(allocator));
     // 之后是一定要建立静态图的。
     if(M == 16){
-        M = 14;
-        ef_construction -= 20;
+        M = 24;
+        ef_construction = 120;
     }
 
     if (!use_static_) {
