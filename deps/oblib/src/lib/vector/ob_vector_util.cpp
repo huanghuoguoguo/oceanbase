ac29b0b20aa8fa57bf5e12d44306c234ba6b3fd0
名为a的全局变量 <1051233107@qq.com>
2024-11-25 20:48:51 +0800

    16 240 120

--- deps/oblib/src/lib/vector/ob_vector_util.cpp
+++ deps/oblib/src/lib/vector/ob_vector_util.cpp
@@ -104,7 +104,7 @@ int create_index(obvectorlib::VectorIndexPtr& index_handler, int index_type,
                                    static_cast<obvectorlib::IndexType>(index_type),
                                    dtype, metric,
                                    dim,
-                                   24,
+                                   16,
                                    240,
                                    120,
                                    allocator);
