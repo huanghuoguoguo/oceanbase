6d5c71581ee4040aed3cbea7156fa20de7a7d4b9
名为a的全局变量 <1051233107@qq.com>
2024-11-26 18:51:08 +0800

    24 200 256

--- deps/oblib/src/lib/vector/ob_vector_util.cpp
+++ deps/oblib/src/lib/vector/ob_vector_util.cpp
@@ -104,9 +104,9 @@ int create_index(obvectorlib::VectorIndexPtr& index_handler, int index_type,
                                    static_cast<obvectorlib::IndexType>(index_type),
                                    dtype, metric,
                                    dim,
-                                   16,
-                                   240,
-                                   300,
+                                   24,
+                                   200,
+                                   256,
                                    allocator);
 #endif
 }
@@ -152,7 +152,7 @@ int knn_search(obvectorlib::VectorIndexPtr index_handler, float* query_vector,in
 #else
   return obvectorlib::knn_search(index_handler, query_vector, dim, topk,
                                  result_dist, result_ids, result_size,
-                                 300, invalid);
+                                 256, invalid);
 #endif
 }
 
