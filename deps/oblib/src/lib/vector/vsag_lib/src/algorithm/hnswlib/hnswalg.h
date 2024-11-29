d5d889f7ccb622c22488210b485e311bd6ae530a
youhh <1051233107@qq.com>
2024-11-29 21:00:50 +0800

    d

--- deps/oblib/src/lib/vector/vsag_lib/src/algorithm/hnswlib/hnswalg.h
+++ deps/oblib/src/lib/vector/vsag_lib/src/algorithm/hnswlib/hnswalg.h
@@ -1660,7 +1660,7 @@ public:
         tableint currObj = enterpoint_node_;
         float curdist = fstdistfunc_(query_data, getDataByInternalId(enterpoint_node_), dist_func_param_);
         // 定义阈值：距离较小的阈值和距离较大的阈值
-        float threshold_near = 800000.0f;  // 距离小于这个值时，减少 efSearch
+        float threshold_near = 80000.0f;  // 距离小于这个值时，减少 efSearch
         float threshold_far = 200000.0f;   // 距离大于这个值时，增加 efSearch
         uint64_t max_ef = static_cast<uint64_t>(10000);
 
