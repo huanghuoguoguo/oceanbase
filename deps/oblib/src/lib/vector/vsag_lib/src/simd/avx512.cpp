61d4e695d9a6f0bb6adc8c7cc83340e43381a6ed
youhh <1051233107@qq.com>
2024-12-02 12:50:44 +0000

    qz

--- deps/oblib/src/lib/vector/vsag_lib/src/simd/avx512.cpp
+++ deps/oblib/src/lib/vector/vsag_lib/src/simd/avx512.cpp
@@ -16,7 +16,6 @@
 
 #include <x86intrin.h>
 #include <iostream>
-#include "../logger.h"
 namespace vsag {
 
 #define PORTABLE_ALIGN32 __attribute__((aligned(32)))
@@ -149,9 +148,9 @@ SQ8ComputeCodesL2Sqr(const void* pVect1v, const void* pVect2v, const void* qty_p
     for (int i = 0; i < 32; ++i) {  
         total_sum += uint16_ptr[i]; // Total sum of squared differences 
     }  
-    vsag::logger::warn("yhh compute result log:{} ",sum);
-    // return static_cast<float>(total_sum);
-    return 0.0f;
+
+    return static_cast<float>(total_sum);
+    // return 0.0f;
 }
 
 
