261f5d236c2d1279ff8562c8f3ed7635b236aa88
youhh <1051233107@qq.com>
2024-12-02 12:58:13 +0000

    sum is float

--- deps/oblib/src/lib/vector/vsag_lib/src/simd/avx512.cpp
+++ deps/oblib/src/lib/vector/vsag_lib/src/simd/avx512.cpp
@@ -140,16 +140,15 @@ SQ8ComputeCodesL2Sqr(const void* pVect1v, const void* pVect2v, const void* qty_p
     }   
  
     // // Sum up all elements in the 512-bit register 
-    int result[16];  // Store results in a 32-element array
+    uint16_t result[32];  // Store results in a 32-element array
     _mm512_storeu_si512(result, sum);  
 
-    int total_sum = 0;  
-    uint16_t* uint16_ptr = (uint16_t*)result;
+    float total_sum = 0.0f;  
     for (int i = 0; i < 32; ++i) {  
-        total_sum += uint16_ptr[i]; // Total sum of squared differences 
+        total_sum += result[i]; // Total sum of squared differences 
     }  
 
-    return static_cast<float>(total_sum);
+    return total_sum;
     // return 0.0f;
 }
 
