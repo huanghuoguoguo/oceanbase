f43a92b21a07ce5be3292c1e3cc8e6e83b079431
youhh <1051233107@qq.com>
2024-12-03 00:57:44 +0000

    32 400 80

--- deps/oblib/src/lib/vector/ob_vector_util.cpp
+++ deps/oblib/src/lib/vector/ob_vector_util.cpp
@@ -104,9 +104,9 @@ int create_index(obvectorlib::VectorIndexPtr& index_handler, int index_type,
                                    static_cast<obvectorlib::IndexType>(index_type),
                                    dtype, metric,
                                    dim,
-                                   18,
-                                   200,
-                                   128,
+                                   32,
+                                   400,
+                                   80,
                                    allocator);
 #endif
 }
@@ -152,7 +152,7 @@ int knn_search(obvectorlib::VectorIndexPtr index_handler, float* query_vector,in
 #else
   return obvectorlib::knn_search(index_handler, query_vector, dim, topk,
                                  result_dist, result_ids, result_size,
-                                 128, invalid);
+                                 80, invalid);
 #endif
 }
 
