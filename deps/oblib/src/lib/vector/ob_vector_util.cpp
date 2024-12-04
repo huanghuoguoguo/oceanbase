a5483d42c5f96a1a2403d3044e1fff656cba5476
youhh <1051233107@qq.com>
2024-12-04 14:09:01 +0800

    quchu dynamic

--- deps/oblib/src/lib/vector/ob_vector_util.cpp
+++ deps/oblib/src/lib/vector/ob_vector_util.cpp
@@ -106,7 +106,7 @@ int create_index(obvectorlib::VectorIndexPtr& index_handler, int index_type,
                                    dim,
                                    20,
                                    200,
-                                   120,
+                                   128,
                                    allocator);
 #endif
 }
@@ -152,7 +152,7 @@ int knn_search(obvectorlib::VectorIndexPtr index_handler, float* query_vector,in
 #else
   return obvectorlib::knn_search(index_handler, query_vector, dim, topk,
                                  result_dist, result_ids, result_size,
-                                 120, invalid);
+                                 128, invalid);
 #endif
 }
 
