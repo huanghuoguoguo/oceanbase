e4e43268677369469be369dca93ce50942bb47eb
youhh <1051233107@qq.com>
2024-12-06 07:43:32 +0000

    log

--- deps/oblib/src/lib/vector/vsag_lib/src/algorithm/hnswlib/hnswalg.h
+++ deps/oblib/src/lib/vector/vsag_lib/src/algorithm/hnswlib/hnswalg.h
@@ -52,7 +52,7 @@ namespace std {
 
                 // 使用位操作提高散列的分布性
                 hc_hash = (hc_hash ^ (hc_hash >> 16)) * 0x9f3b;
-                hc_hash = (hc_hash ^ (hc_hash >> 16));
+                hc_hash = (hc_hash ^ (hc_hash >> 16)) * 0xfd34;
             }
             return hc_hash;
         }
@@ -95,7 +95,8 @@ private:
     ///  缓存实现
     std::bitset<HC_BIT_SIZE> hc_cache_bs_;
     std::unordered_map<std::array<int8_t, DIM>, std::array<std::pair<float, labeltype>, 10>, std::hash<std::array<int8_t, DIM>>> hc_cache_res_;
-    bool use_cache = false;
+    bool use_cache = true;
+
     double mult_{0.0}, revSize_{0.0};
     int maxlevel_{0};
 
@@ -1721,24 +1722,24 @@ public:
         auto vector = (float*)query_data;
  
         std::array<int8_t, DIM> sq8 = {0};
-        int hc_hash = 0;
+        // int hc_hash = 0;
         for (size_t i = 0; i < DIM; ++i) {
             float value = vector[i];
             
             // 将每个 float 值转换为 uint8_t
             sq8[i] = static_cast<int8_t>(value);
             
-            // 第一轮哈希，使用加权值
-            hc_hash = hc_hash * 31 + sq8[i];
+            // // 第一轮哈希，使用加权值
+            // hc_hash = hc_hash * 31 + sq8[i];
 
-            // 使用位操作提高散列的分布性
-            hc_hash = (hc_hash ^ (hc_hash >> 16)) * 0x9f3b;
-            hc_hash = (hc_hash ^ (hc_hash >> 16));
+            // // 使用位操作提高散列的分布性
+            // hc_hash = (hc_hash ^ (hc_hash >> 16)) * 0x9f3b;
+            // hc_hash = (hc_hash ^ (hc_hash >> 16));
         }
 
 
         // 使用最终的哈希值，确保它不超过 HC_BIT_SIZE
-        hc_hash = hc_hash % HC_BIT_SIZE;
+        // hc_hash = hc_hash % HC_BIT_SIZE;
         // 如果该hc_hash存在，尝试走缓存。
 
         // if (k != 10000 && hc_cache_bs_.test(hc_hash)) {
@@ -1750,6 +1751,9 @@ public:
 
                 // 将缓存结果转为返回格式
                 std::vector<std::pair<float, labeltype>> result;
+                if(result.size() != 10){
+                    vsag::logger::warn("yhh log find size: {}", result.size());
+                }
                 for (size_t i = 0; i < cached_result.size(); ++i) {
                     const auto& [cached_dist, cached_label] = cached_result[i];
                     result.emplace_back(cached_dist, cached_label);
@@ -1825,8 +1829,11 @@ public:
                 candidates[i].second = getExternalLabel(candidates[i].second);
                 result[i] = {candidates[i].first, candidates[i].second};
             }
+            if(result.size() != 10){
+                    vsag::logger::warn("yhh log add size: {}", result.size());
+            }
             hc_cache_res_.emplace(sq8, result);
-            hc_cache_bs_.set(hc_hash);
+            // hc_cache_bs_.set(hc_hash);
         }
         
         return std::move(candidates);
