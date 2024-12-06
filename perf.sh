7ec8d095b0ef5bab2a69885f2504cb6d7fed5454
youhh <1051233107@qq.com>
2024-12-06 12:13:34 +0000

    perf

---
+++ perf.sh
@@ -0,0 +1,18 @@
+
+
+cd /root/source/ann-benchmarks
+
+python run.py --algorithm oceanbase --local --force --dataset sift-128-euclidean --runs 1 --skip_fit
+python plot.py --dataset sift-128-euclidean --recompute
+
+python run.py --algorithm oceanbase --local --force --dataset sift-128-euclidean --runs 1 --skip_fit
+python plot.py --dataset sift-128-euclidean --recompute
+
+python run.py --algorithm oceanbase --local --force --dataset sift-128-euclidean --runs 1 --skip_fit
+python plot.py --dataset sift-128-euclidean --recompute
+
+cd /root/source/ann-benchmarks/ann_benchmarks/algorithms/oceanbase
+python hybrid_ann.py
+
+
+
