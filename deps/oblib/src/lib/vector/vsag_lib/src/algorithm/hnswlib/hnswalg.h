15ba64948c1351c678d6c8785af5f0a81ee11f72
youhh <1051233107@qq.com>
2024-12-08 04:40:41 +0000

    use cache

--- deps/oblib/src/lib/vector/vsag_lib/src/algorithm/hnswlib/hnswalg.h
+++ deps/oblib/src/lib/vector/vsag_lib/src/algorithm/hnswlib/hnswalg.h
@@ -90,7 +90,7 @@ private:
 
     ///  缓存实现
     std::map<std::array<uint8_t, DIM>, std::array<std::pair<float, labeltype>, 10>> hc_cache_res_;
-    bool use_cache = false;
+    bool use_cache = true;
 
     double mult_{0.0}, revSize_{0.0};
     int maxlevel_{0};
