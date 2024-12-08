9606d145a1078df819c71d0c1b81835c0218a2d0
youhh <1051233107@qq.com>
2024-12-08 15:02:21 +0800

    update sh

--- cp_skip.sh
+++ cp_skip.sh
@@ -42,6 +42,14 @@ python plot.py --dataset sift-128-euclidean --recompute
 python run.py --algorithm oceanbase --local --force --dataset sift-128-euclidean --runs 1 --skip_fit
 python plot.py --dataset sift-128-euclidean --recompute
 
+cd /root/source/oceanbase
+./tools/deploy/obd.sh restart -n obcluster
+cd /root/source/ann-benchmarks
+
+
+python run.py --algorithm oceanbase --local --force --dataset sift-128-euclidean --runs 1 --skip_fit
+python plot.py --dataset sift-128-euclidean --recompute
+
 cd /root/source/ann-benchmarks/ann_benchmarks/algorithms/oceanbase
 python hybrid_ann.py
 
