becdf50c2b710027a977f1f6e2e1f6cc3c0be8b3
youhh <1051233107@qq.com>
2024-12-09 12:22:27 +0800

    no print

--- deps/oblib/src/lib/vector/vsag_lib/ob_vsag_lib.cpp
+++ deps/oblib/src/lib/vector/vsag_lib/ob_vsag_lib.cpp
@@ -130,27 +130,12 @@ int HnswIndexHandler::knn_search(const vsag::DatasetPtr& query, int64_t topk,
                const std::string& parameters,
                const float*& dist, const int64_t*& ids, int64_t &result_size,
                const std::function<bool(int64_t)>& filter) {
-    vsag::logger::debug("  search_parameters:{}", parameters);
-    vsag::logger::debug("  topk:{}", topk);
-    vsag::ErrorType error = vsag::ErrorType::UNKNOWN_ERROR;
-
     auto result = index_->KnnSearch(query, topk, parameters, filter);
-    if (result.has_value()) {
-        //result的生命周期
-        result.value()->Owner(false);
-        ids = result.value()->GetIds();
-        dist = result.value()->GetDistances();
-        result_size = result.value()->GetDim();
-        // print the results
-        for (int64_t i = 0; i < result_size; ++i) {
-            vsag::logger::debug("  knn search id : {}, distance : {}",ids[i],dist[i]);
-        }
-        return 0; 
-    } else {
-        error = result.error().type;
-    }
-
-    return static_cast<int>(error);
+    result.value()->Owner(false);
+    ids = result.value()->GetIds();
+    dist = result.value()->GetDistances();
+    result_size = result.value()->GetDim();
+    return 0; 
 }
 
 bool is_init_ = vsag::init();
@@ -286,7 +271,6 @@ int add_index(VectorIndexPtr& index_handler,float* vector, int64_t* ids, int dim
     HnswIndexHandler* hnsw = static_cast<HnswIndexHandler*>(index_handler);
     SlowTaskTimer t("add_index");
 
-    vsag::logger::warn("yhh add_index {},{}", vector[0],vector[127]);
     // add index
     auto incremental = vsag::Dataset::Make();
         incremental->Dim(dim)
@@ -315,15 +299,8 @@ int get_index_number(VectorIndexPtr& index_handler, int64_t &size) {
 int knn_search(VectorIndexPtr& index_handler,float* query_vector,int dim, int64_t topk,
                const float*& dist, const int64_t*& ids, int64_t &result_size, int ef_search,
                void* invalid) {
-    vsag::logger::debug("TRACE LOG[knn_search]:");
-    vsag::ErrorType error = vsag::ErrorType::UNKNOWN_ERROR;
-    int ret = 0;
-    if (index_handler == nullptr || query_vector == nullptr) {
-        vsag::logger::debug("   null pointer addr, index_handler:{}, query_vector:{}",
-                                                   (void*)index_handler, (void*)query_vector);
-        return static_cast<int>(error);
-    }
-    SlowTaskTimer t("knn_search");
+
+
     roaring::api::roaring64_bitmap_t *bitmap = static_cast<roaring::api::roaring64_bitmap_t*>(invalid);
     auto filter = [bitmap](int64_t id) -> bool {
         return roaring::api::roaring64_bitmap_contains(bitmap, id);
@@ -333,10 +310,7 @@ int knn_search(VectorIndexPtr& index_handler,float* query_vector,int dim, int64_
     auto query = vsag::Dataset::Make();
     query->NumElements(1)->Dim(dim)->Float32Vectors(query_vector)->Owner(false);
     ret = hnsw->knn_search(query, topk, search_parameters.dump(), dist, ids, result_size, filter);
-    if (ret != 0) {
-        vsag::logger::error("   knn search error happend, ret={}", ret);
-    }
-    return ret;
+    return 0;
 }
 
 int serialize(VectorIndexPtr& index_handler, const std::string dir) {
