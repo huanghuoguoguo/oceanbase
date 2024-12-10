fbf6caaa1a77a67a410cab9b647a59ee126347ca
youhh <1051233107@qq.com>
2024-12-10 13:59:11 +0800

    f

--- deps/oblib/src/lib/vector/vsag_lib/src/index/hnsw.cpp
+++ deps/oblib/src/lib/vector/vsag_lib/src/index/hnsw.cpp
@@ -331,7 +331,7 @@ HNSW::knn_search(const DatasetPtr& query,
         try {
             auto hnsw = reinterpret_cast<hnswlib::HierarchicalNSW*>(alg_hnsw.get());
             key_results = hnsw->searchKnn(
-                (const void*)vector.data(), key_scan_k, key_scan_ef, filter_ptr);
+                (const void*)vector, key_scan_k, key_scan_ef, filter_ptr);
         } catch (const std::runtime_error& e) {
             LOG_ERROR_AND_RETURNS(ErrorType::INTERNAL_ERROR,
                                   "failed to perofrm knn_search(internalError): ",
